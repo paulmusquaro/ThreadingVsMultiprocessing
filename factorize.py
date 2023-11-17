b1ca0bb78e350aa92af08b4585c99e53cd3ed111
paulmusquaro <136118097+paulmusquaro@users.noreply.github.com>
2023-11-17 14:07:48 +0200

    blablabla

---
+++ factorize.py
@@ -0,0 +1,32 @@
+import time
+from multiprocessing import Pool, cpu_count
+
+def factorize(number):
+    factors = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            factors.append(i)
+    return factors
+
+def factorize_sync(numbers):
+    return [factorize(number) for number in numbers]
+
+def factorize_parallel(numbers):
+    with Pool(cpu_count()) as pool:
+        results = pool.map(factorize, numbers)
+    return results
+
+def measure_time(func, *args):
+    start_time = time.time()
+    result = func(*args)
+    end_time = time.time()
+    return result, end_time - start_time
+
+if __name__ == "__main__":
+    numbers = [128, 255, 99999, 10651060]
+
+    result_sync, time_sync = measure_time(factorize_sync, numbers)
+    print(f"Sync factorize for {numbers}: {result_sync}, Time: {time_sync:.4f} seconds")
+
+    result_parallel, time_parallel = measure_time(factorize_parallel, numbers)
+    print(f"Parallel factorize for {numbers}: {result_parallel}, Time: {time_parallel:.4f} seconds")
---
+++ filesorter.py
@@ -0,0 +1,55 @@
+import sys
+from pathlib import Path
+from shutil import copyfile
+from threading import Thread
+import logging
+
+source_folder = None
+output_folder = "dist"
+
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "--source" or sys.argv[i] == "-s":
+        source_folder = sys.argv[i + 1]
+    elif sys.argv[i] == "--output" or sys.argv[i] == "-o":
+        output_folder = sys.argv[i + 1]
+
+if source_folder is None:
+    print("Потрібно вказати шлях до директорії --source")
+    sys.exit(1)
+
+source = Path(source_folder)
+output = Path(output_folder)
+folders = []
+
+def grabs_folder(path: Path) -> None:
+    for el in path.iterdir():
+        if el.is_dir():
+            folders.append(el)
+            grabs_folder(el)
+
+def copy_file(path: Path) -> None:
+    for el in path.iterdir():
+        if el.is_file():
+            ext = el.suffix[1:]
+            ext_folder = output / ext
+            try:
+                ext_folder.mkdir(exist_ok=True, parents=True)
+                copyfile(el, ext_folder / el.name)
+            except OSError as err:
+                logging.error(err)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
+
+    folders.append(source)
+    grabs_folder(source)
+    print(folders)
+
+    threads = []
+    for folder in folders:
+        th = Thread(target=copy_file, args=(folder,))
+        th.start()
+        threads.append(th)
+
+    [th.join() for th in threads]
+    print(f"Можна видалити {source}")
